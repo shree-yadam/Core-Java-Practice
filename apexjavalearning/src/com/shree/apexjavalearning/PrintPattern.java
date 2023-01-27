604766cf50efd6f02868bde5ec8142eb0156b845
Shree Devi Yadam <shree.yadam@gmail.com>
2023-01-26 22:41:51 -0800

    Print left triangle star pattern of given depth

--- apexjavalearning/src/com/shree/apexjavalearning/PrintPattern.java
+++ apexjavalearning/src/com/shree/apexjavalearning/PrintPattern.java
@@ -2,11 +2,24 @@ package com.shree.apexjavalearning;
 
 public class PrintPattern {
 	public static void main(String[] args) {
-		printRightTriangleStarPattern(8);
+		//printRightTriangleStarPattern(8);
+		printleftTriangleStarPattern(8);
 	}
 	
 	public static void printRightTriangleStarPattern(int depth) {
 		for(int i = 0; i < depth; i++) {
+			for(int j = 0; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+	}
+	
+	public static void printleftTriangleStarPattern(int depth) {
+		for(int i = 0; i < depth; i++) {
+			for(int j = 0; j < depth - i; j++) {
+				System.out.print("  ");
+			}
 			for(int j = 0; j <= i; j++) {
 				System.out.print("* ");
 			}
