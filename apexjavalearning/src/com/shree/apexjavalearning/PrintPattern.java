7350cddd00695328e512b005f0d94a382e8fc0fa
Shree Devi Yadam <shree.yadam@gmail.com>
2023-01-26 22:44:27 -0800

    Print pyramid star pattern of given depth

--- apexjavalearning/src/com/shree/apexjavalearning/PrintPattern.java
+++ apexjavalearning/src/com/shree/apexjavalearning/PrintPattern.java
@@ -3,7 +3,8 @@ package com.shree.apexjavalearning;
 public class PrintPattern {
 	public static void main(String[] args) {
 		//printRightTriangleStarPattern(8);
-		printleftTriangleStarPattern(8);
+		//printLeftTriangleStarPattern(8);
+		printPyramidStarPattern(6);
 	}
 	
 	public static void printRightTriangleStarPattern(int depth) {
@@ -15,10 +16,22 @@ public class PrintPattern {
 		}
 	}
 	
-	public static void printleftTriangleStarPattern(int depth) {
+	public static void printLeftTriangleStarPattern(int depth) {
 		for(int i = 0; i < depth; i++) {
 			for(int j = 0; j < depth - i; j++) {
 				System.out.print("  ");
+			}
+			for(int j = 0; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+	}
+	
+	public static void printPyramidStarPattern(int depth) {
+		for(int i = 0; i < depth; i++) {
+			for(int j = 0; j < depth - i; j++) {
+				System.out.print(" ");
 			}
 			for(int j = 0; j <= i; j++) {
 				System.out.print("* ");
