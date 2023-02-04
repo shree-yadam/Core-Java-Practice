0ccba6702ffdcdd6dac512dd1f363698ef6a9cf6
Shree Devi Yadam <shree.yadam@gmail.com>
2023-02-04 11:08:53 -0800

    Try first program with Comprehensice JAVA

---
+++ apexjavalearning/src/com/shree/comprehensivejava/Example.java
@@ -0,0 +1,12 @@
+package com.shree.comprehensivejava;
+
+/*
+ * This is a simple JAVA program
+ * Call this file Example.java
+ */
+public class Example {
+	// Your program begins with a call to main
+	public static void main(String[] args) {
+		System.out.println("This is a simple JAVA program");
+	}
+}
