87a36efe1e4afb62a2c3654665a513edf19fedf9
Shree Devi Yadam <shree.yadam@gmail.com>
2023-02-08 18:48:42 -0800

    Assign values to array indices and display

--- apexjavalearning/src/com/shree/apexjavalearning/TestArray.java
+++ apexjavalearning/src/com/shree/apexjavalearning/TestArray.java
@@ -6,6 +6,11 @@ public class TestArray {
 		
 		int[] otherArr = new int[] {1, 2, 3};
 		System.out.println(array[3]);
-		System.out.println(otherArr[3]);// ArrayIndexOutOfBounds Exception
+		//System.out.println(otherArr[3]);// ArrayIndexOutOfBounds Exception
+		int value = 100;
+		for(int i = 0; i < array.length; i++) {
+			array[i] = value + i;
+			System.out.println(array[i]);
+		}
 	}
 }
