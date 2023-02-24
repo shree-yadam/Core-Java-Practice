3f52c18fc8ac755632c558b0df55cf7d98ff0839
Shree Devi Yadam <shree.yadam@gmail.com>
2023-02-23 20:46:09 -0800

    Add volume method to Box class

---
+++ comprehensivejava/BoxDemo3.java
@@ -0,0 +1,36 @@
+// This program includes a method inside the box class.
+
+class Box {
+    double width;
+    double height;
+    double depth;
+
+    // display volume of a box
+    void volume() {
+        System.out.print("Volume is ");
+        System.out.println(width * height * depth);
+    }
+}
+
+class BoxDemo3 {
+    public static void main(String[] args) {
+        Box myBox1 = new Box();
+        Box myBox2 = new Box();
+
+        // assign values to myBox1's instance variables
+        myBox1.width = 10;
+        myBox1.height = 20;
+        myBox1.depth = 15;
+
+        /* assign different values to myBox2's instance variables */
+        myBox2.width = 3;
+        myBox2.height = 6;
+        myBox2.depth = 9;
+
+        // display volume of first box
+        myBox1.volume();
+
+        // display volume of second box
+        myBox2.volume();
+    }
+}
