771d591b80ec6bfa1b3c4a2d048a21bafa76cc32
Shree Devi Yadam <shree.yadam@gmail.com>
2023-02-24 21:24:56 -0800

    Demonstrate method overload usage where parameter type is not exact match but compatible

--- comprehensivejava/Overload.java
+++ comprehensivejava/Overload.java
@@ -4,33 +4,46 @@ class OverloadDemo {
         System.out.println("No parameters");
     }
 
-    // Overload test for one integer parameter.
+    /*// Overload test for one integer parameter.
     void test (int a) {
         System.out.println("a: " + a);
-    }
+    }*/
 
     // Overload test for two integer parameters.
     void test (int a, int b) {
         System.out.println("a and b: " + a + " " + b);
     }
 
-    // Overload test for a double parameter.
+    /*// Overload test for a double parameter.
     double test (double a) {
         System.out.println("double a: " + a);
         return a * a;
+    }*/
+
+    // Overload test for a double parameter
+    void test(double a) {
+        System.out.println("Inside test(double) a: " + a);
     }
 }
 
 class Overload {
     public static void main(String[] args) {
         OverloadDemo ob = new OverloadDemo();
-        double result;
+        /*double result;
 
         // call all versions of test ()
         ob.test();
         ob.test(10);
         ob.test(10, 20);
         result = ob.test(123.25);
-        System.out.println("Result of ob.test(123.25) : " + result);
+        System.out.println("Result of ob.test(123.25) : " + result);*/
+
+        int i = 88;
+
+        ob.test();
+        ob.test(10, 20);
+
+        ob.test(i); // this will invoke test(double)
+        ob.test(123.2); // this will invoke test(double)
     }
 }
