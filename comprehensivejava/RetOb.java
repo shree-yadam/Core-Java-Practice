be448cac0e4271d483e4233de5e013b3b02ac3d2
Shree Devi Yadam <shree.yadam@gmail.com>
2023-02-25 23:11:37 -0800

    Demonstrate returning an objet

--- comprehensivejava/RetOb.java
+++ comprehensivejava/RetOb.java
@@ -19,5 +19,8 @@ public class RetOb {
         ob2 = ob1.incrByTen();
         System.out.println("ob1.a: " + ob1.a);
         System.out.println("ob2.a: " + ob2.a);
+
+        ob2 = ob2.incrByTen();
+        System.out.println("ob2.a after second increase: " + ob2.a);
     }
 }
