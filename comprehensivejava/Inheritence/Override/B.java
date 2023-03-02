0f9da9be270b13bd2a31f7fc3b02950ff6d6c662
Shree Devi Yadam <shree.yadam@gmail.com>
2023-03-01 22:14:50 -0800

    Method overload vs override

--- comprehensivejava/Inheritence/Override/B.java
+++ comprehensivejava/Inheritence/Override/B.java
@@ -8,10 +8,15 @@ class B extends A{
         super(a, b);
         k = c;
     }
-
+/*
     // display k - this overrides show() in A
     void show() {
         super.show();
         System.out.println("k: " + k);
+    }*/
+
+    // overload show()
+    void show(String msg) {
+        System.out.println(msg + k);
     }
 }
--- comprehensivejava/Inheritence/Override/Override.java
+++ comprehensivejava/Inheritence/Override/Override.java
@@ -4,6 +4,7 @@ class Override {
     public static void main(String[] args) {
         B subOb = new B(1, 2, 3);
 
-        subOb.show();
+        subOb.show("This is k: "); // This calls show in B
+        subOb.show(); // this calls show in A
     }
 }
