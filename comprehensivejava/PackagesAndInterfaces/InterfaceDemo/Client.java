1055adfc600ee2c017d20710afc59fe1810a7566
Shree Devi Yadam <shree.yadam@gmail.com>
2023-03-03 21:50:26 -0800

    Add non interface method to implementing class

--- comprehensivejava/PackagesAndInterfaces/InterfaceDemo/Client.java
+++ comprehensivejava/PackagesAndInterfaces/InterfaceDemo/Client.java
@@ -7,4 +7,9 @@ public class Client implements Callback{
     public void callback(int p) {
         System.out.println("callback called with " + p);
     }
+
+    void nonIfaceMethod() {
+        System.out.println("Classes that implement interfaces " +
+                "may also define other members, too.");
+    }
 }
