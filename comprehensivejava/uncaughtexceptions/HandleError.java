2443037ceb50346e95ca27e484bf65b792590eb7
Shree Devi Yadam <shree.yadam@gmail.com>
2023-03-10 21:11:20 -0800

    Refactor for catch blcok to display description of exception

--- comprehensivejava/uncaughtexceptions/HandleError.java
+++ comprehensivejava/uncaughtexceptions/HandleError.java
@@ -11,7 +11,7 @@ public class HandleError {
                 c = r.nextInt();
                 a = 12345 / (b/c);
             } catch (ArithmeticException e){
-                System.out.println("Division by zero.");
+                System.out.println("Exception: " + e);
                 a = 0; // Set a to zero and continue
             }
             System.out.println("a: " + a);
