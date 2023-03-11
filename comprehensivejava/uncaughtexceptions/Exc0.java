d1901962aa7497a77a68e77493b7ee7a384a6b09
Shree Devi Yadam <shree.yadam@gmail.com>
2023-03-10 18:45:58 -0800

    Demo logs for exceptione thrown from sub routine

--- comprehensivejava/uncaughtexceptions/Exc0.java
+++ comprehensivejava/uncaughtexceptions/Exc0.java
@@ -1,7 +1,10 @@
 
 public class Exc0 {
-    public static void main(String[] args) {
+    static void subroutine() {
         int d = 0;
         int a = 42/d;
+    }
+    public static void main(String[] args) {
+        Exc0.subroutine();
     }
 }
